680ab266719d1b8f6ec01549ddf6246e5f1fd63c
rayan-ahamadi <115161695+rayan-ahamadi@users.noreply.github.com>
2023-09-12 10:24:00 +0200

    jour 04

---
+++ Jour04/Job05/ExecutorService.java
@@ -0,0 +1,16 @@
+package Job05;
+
+public class ExecutorService implements Runnable {
+   
+
+    @Override
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
